f4c1ce3d2e5d9824f5fca6688646454d920922dc
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-07-04 23:39:42 +0530

    added PUT functionality

--- main.js
+++ main.js
@@ -57,6 +57,20 @@ const express = require('express');
       res.status(400).send('Invalid request');
     }
   });
+
+  // PUT /todos/:id - Update an existing todo item by ID
+  app.put('/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const { title, description } = req.body;
+    const index = todos.findIndex(todo => todo.id === id);
+    if (index !== -1) {
+      todos[index] = { id, title, description };
+      saveTodos();
+      res.status(200).send('Todo updated');
+    } else {
+      res.status(404).send('Todo not found');
+    }
+  });
   
   // Handle undefined routes
   app.use((req, res) => {
